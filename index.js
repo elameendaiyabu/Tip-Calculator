d7e97c600e0971b0ebf0ee6c34d5da889f4b3bc3
Elameen Daiyabu <elameendaiyabu@gmail.com>
2024-02-12 20:49:18 +0100

    finished project

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
+  //DOM elements assignments and variables definitin
   const bill = document.getElementById("bill-value");
   const people = document.getElementById("people-value");
   const errorMessage = document.getElementById("error-message");
@@ -17,8 +18,12 @@ document.addEventListener("DOMContentLoaded", () => {
   let isBill = false;
   let isPeople = false;
   let isPercentage = false;
+
+  //An array containing my percentages values which gets linked to the radio button percentages.
+  //Value has to match the id of each respective radio button
   const percentageArray = [5, 10, 15, 20, 25];
 
+  //function that renders the amount per person and total per person
   function render() {
     if (isBill && isPeople && isPercentage) {
       let amountValue = ((billAmount / 100) * percentage) / peopleAmount;
@@ -32,6 +37,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
+  //function that enables the reset button and changes everything to default
   function reset() {
     resets.style.backgroundColor = "cyan";
     resets.addEventListener("click", () => {
@@ -50,10 +56,13 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 
+  //Adds the success class to the input field when the input field is focused.
   bill.addEventListener("focus", () => {
     bill.classList.add("success");
   });
 
+  //Event listener for the bill input field. if > 0, border changes to green else it is red. Also gets the bill amount.
+  //If bill amount is 0, it will display an error message.
   bill.addEventListener("keyup", () => {
     if (bill.value > 0) {
       errorMessage.textContent = "";
@@ -63,7 +72,6 @@ document.addEventListener("DOMContentLoaded", () => {
       isBill = true;
       billAmount = bill.value;
       parseFloat(billAmount);
-      console.log(billAmount);
     } else {
       bill.classList.add("error");
       bill.style.borderColor = "red";
@@ -72,10 +80,13 @@ document.addEventListener("DOMContentLoaded", () => {
     render();
   });
 
+  //Adds the success class to the input field when the input field is focused.
   people.addEventListener("focus", () => {
     people.classList.add("success");
   });
 
+  //Event listener for the people input field. if > 0, border changes to green else it is red. Also gets the people amount.
+  //If people amount is 0, it will display an error message.
   people.addEventListener("keyup", () => {
     if (people.value > 0) {
       errorMessage2.textContent = "";
@@ -85,7 +96,6 @@ document.addEventListener("DOMContentLoaded", () => {
       isPeople = true;
       peopleAmount = people.value;
       parseFloat(peopleAmount);
-      console.log(peopleAmount);
     } else {
       people.classList.add("error");
       people.style.borderColor = "red";
@@ -94,29 +104,33 @@ document.addEventListener("DOMContentLoaded", () => {
     render();
   });
 
+  //Gets the percentage value from the radio buttons.
   radios.forEach((radio) => {
     radio.addEventListener("change", () => {
       percentageArray.forEach((value, index) => {
         if (radio.checked && radio.id === `${value}`) {
           isPercentage = true;
           percentage = value;
-          console.log(percentage);
         }
       });
       render();
     });
   });
 
+  //Adds the success class to the custom input field when the input field is focused.
   customPercentage.addEventListener("focus", () => {
     customPercentage.classList.add("success");
   });
 
+  //Event click listener to the custom button. It hides the custom buttton and shows the custom percentage input field.
   custom.addEventListener("click", function () {
     customContainer.style.display = "none";
     customPercentage.style.display = "block";
     radios.checked = "";
   });
 
+  //Event listener for the custom percentage input field. if > 0, border changes to green else it is red.
+  //Also gets the custom percentage amount.
   customPercentage.addEventListener("keyup", () => {
     if (customPercentage.value > 0) {
       errorMessage2.textContent = "";
